31c7288b92d4e819a07a7f13fb03f871113ee32f
Siddhant Gupta <57240917+Siddhant69138@users.noreply.github.com>
2023-11-03 11:00:37 +0000

    post and prefix

--- .devcontainer/01_basics/03_conversionOperation.js
+++ .devcontainer/01_basics/03_conversionOperation.js
@@ -1,11 +1,11 @@
 let score="33abc"
 
-console.log(typeof(score));
+//console.log(typeof(score));
 
 let valueInNumber=Number(score);
 
-console.log(typeof valueInNumber);
-console.log(valueInNumber);
+//console.log(typeof valueInNumber);
+//console.log(valueInNumber);
 
 //"33"=>33
 //"33abc" =>NaN(Not a Number)
@@ -14,12 +14,43 @@ console.log(valueInNumber);
 let isLoggedIn="Sid"
 
 let booleanIsLoggedIn=Boolean(isLoggedIn)
-console.log(booleanIsLoggedIn)
+//console.log(booleanIsLoggedIn)
 //1 =>true
 //0 =>false
 //"Sid"=> true
 // ""=>false
 let someNumber=33
 let stringNumber=String(someNumber)
-console.log(stringNumber);
-console.log(typeof stringNumber);
+//console.log(stringNumber);
+//console.log(typeof stringNumber);
+
+//#############################Operations#####################
+
+
+let str1="hello"
+let str2=" Siddhant"
+let str3=str1+str2
+// console.log(str3)
+
+// console.log("1"+2);
+// console.log(1+"2");
+// console.log("1"+2+2);
+// console.log(1+2+"2");
+
+// console.log(+true);
+// console.log(+"");
+
+let gameCounter=100
+++gameCounter;
+console.log(gameCounter)
+
+let x=3;
+const y=x++;
+console.log(x);
+console.log(y);
+
+let a=3;
+const b=++a;
+console.log(a);
+console.log(b);
+
