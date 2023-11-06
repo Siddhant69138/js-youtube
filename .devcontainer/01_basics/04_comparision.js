23e5f539e1bdc1ac7db0a19e3072c34d5c7c2a93
Siddhant Gupta <57240917+Siddhant69138@users.noreply.github.com>
2023-11-06 05:54:39 +0000

    comparision

---
+++ .devcontainer/01_basics/04_comparision.js
@@ -0,0 +1,11 @@
+//  console.log(null>0);
+//  console.log(null==0);
+//  console.log(null>=0);
+
+//  console.log(undefined==0);
+//  console.log(undefined<0);
+//  console.log(undefined>0);
+// -- avoid using this;
+
+
+console.log(2==="2");
