e7de9204724e71cde27dbb81b9fb58164942ee17
Siddhant Gupta <57240917+Siddhant69138@users.noreply.github.com>
2023-11-07 05:48:59 +0000

    data type summary

--- .devcontainer/01_basics/datatype-summary.js
+++ .devcontainer/01_basics/datatype-summary.js
@@ -1,44 +1,70 @@
-//primitive- String,number,boolean,BigInt,undefined,null,Symbol
+// //primitive- String,number,boolean,BigInt,undefined,null,Symbol
 
-const score=100
-const scoreValue=100.3
-console.log( typeof scoreValue)
+// const score=100
+// const scoreValue=100.3
+// console.log( typeof scoreValue)
 
-const isLoggedIn=true
-console.log(typeof isLoggedIn)
+// const isLoggedIn=true
+// console.log(typeof isLoggedIn)
 
-const outsideTemp= null
-console.log(typeof outsideTemp) //datatype null=>object
-let userEmail;
-console.log(typeof userEmail)
-const id=Symbol('123')
-const anotherId=Symbol('123')
-console.log(typeof id)
-console.log(id===anotherId)
+// const outsideTemp= null
+// console.log(typeof outsideTemp) //datatype null=>object
+// let userEmail;
+// console.log(typeof userEmail)
+// const id=Symbol('123')
+// const anotherId=Symbol('123')
+// console.log(typeof id)
+// console.log(id===anotherId)
 
-//const bigInt=12482782849n
+// //const bigInt=12482782849n
 
 
-//---------Reference(Non Primitive)
+// //---------Reference(Non Primitive)
 
-//Array,Objects,Function
+// //Array,Objects,Function
 
-const cousinName=['Siddhant','Anuj','Smita'];
-console.log(typeof cousinName)
+// const cousinName=['Siddhant','Anuj','Smita'];
+// console.log(typeof cousinName)
 
-//object--key,value pair
-let myObj={
-    name:"Siddhant",
-    age:24
+// //object--key,value pair
+// let myObj={
+//     name:"Siddhant",
+//     age:24
+// }
+
+
+// console.log(typeof myObj)
+
+// const Myfunction =function(){
+//     console.log("hello World")
+// }
+
+// console.log(typeof Myfunction)
+
+//+++++++++++++++++++++++++++++++++++
+
+//Stack memory used-Primitive--copy milega
+
+
+//Heap memory---Non Primitive(Reference in original value)
+
+let myYoutubeName="Sid"
+
+let anotherName=myYoutubeName
+
+anotherName="chaiaurSid"
+
+console.log(anotherName);
+console.log(myYoutubeName);
+
+let user1={
+    email:"abc@gmail.com",
+    UPI:"12345@ybl"
 }
 
+let user2=user1
+console.log(user1)
+console.log(user2)
 
-console.log(typeof myObj)
-
-const Myfunction =function(){
-    console.log("hello World")
-}
-
-console.log(typeof Myfunction)
 
 
