a7740183aa5d6001e184703dbc98117894298ecd
Siddhant Gupta <57240917+Siddhant69138@users.noreply.github.com>
2023-11-19 07:56:41 +0000

    function with object and array

--- .devcontainer/03_basics/01_functions.js
+++ .devcontainer/03_basics/01_functions.js
@@ -29,7 +29,51 @@ function loginUserMessage(username){
     return `${username} just logged in`
 }
 
-console.log(loginUserMessage())
+// console.log(loginUserMessage())
+
+
+//-----------------------
+
+//cart--logic
+function calculateCartPrice(...num1){
+    return num1
+}
+
+console.log(calculateCartPrice(200,500,300,5000))
+
+function calculateCartPrice1(n1,n2,...num1){
+    return num1
+}
+
+console.log(calculateCartPrice1(200,500,300,5000))
+
+//----passing object in fun
+const user={
+    username:"Siddhant",
+    price:1000
+}
+
+function handleObject(anyobject){
+    console.log(`Username is ${anyobject.username} and price is ${anyobject.price} `)
+
+}
+
+//handleObject(user)
+
+handleObject({
+    username:"sam",
+    price:399
+})
+
+//passing array in fun
+
+const myNewArray=[200,400,500]
+
+function returnSecondArray(getArray){
+    return getArray[1]
+}
+console.log(returnSecondArray(myNewArray));
+console.log(returnSecondArray([200,400,500]));
 
 
 
