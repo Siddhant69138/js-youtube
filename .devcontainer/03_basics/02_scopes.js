50207615007ab1263209922f48f31acc65cada4f
Siddhant Gupta <57240917+Siddhant69138@users.noreply.github.com>
2023-11-20 06:00:49 +0000

    scope level

--- .devcontainer/03_basics/02_scopes.js
+++ .devcontainer/03_basics/02_scopes.js
@@ -6,7 +6,7 @@ if (true){
     let a=10
     const b=20
     //var c=30 //so we won't study var as it comes out of scope so will cause problem
-    console.log("INNER",a)
+    //console.log("INNER",a)
     
 }
 
@@ -14,3 +14,45 @@ if (true){
 console.log(a);
 //console.log(b);
 //console.log(c);
+
+//--nested scope
+
+function one(){
+    const username="siddhant"
+
+    function two(){
+        const website="youtube"
+        console.log(username)
+    }
+    //console.log(website);
+    two()
+}
+//one()
+
+if(true){
+    const username="siddhant"
+    if(username==="siddhant"){
+        const website=" youtube"
+        //console.log(username+ website)
+    }
+    //console.log(website)
+}
+//console.log(username)
+
+
+//+++++++++++++interesting+++++++++++
+
+
+
+function addOne(num){
+    return num+1
+
+}
+
+addOne(5)
+
+const addTwo=function(num){
+    return num+2
+}
+
+console.log(addTwo(5))
