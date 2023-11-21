40bdf6c9b9cefe7718b90f58ae905a06fb5b198f
Siddhant Gupta <57240917+Siddhant69138@users.noreply.github.com>
2023-11-21 05:47:35 +0000

    while do while

---
+++ .devcontainer/05_iterators/two.js
@@ -0,0 +1,23 @@
+//while
+// let index=0
+// while (index<=10) {
+//     console.log(`value of index is ${index}`);
+//     index=index+2
+
+    
+// }
+
+let myArray=['abc','def','ghi']
+let arr=0;
+while(arr<=myArray.length){
+    //console.log(`Value is ${arr}`);
+    arr=arr+1;
+}
+//------------------do while
+let score=11
+do {
+    console.log(`score is ${score}`);
+    score++;
+} while (score<=10);
+//do while is like udhar, ak bar look chalega hi
+
